c477cc9c9ab5ce358ce59b355a26fce6146127bd
Alvaro <aguachilema@gmail.com>
2026-05-01 15:58:03 -0500

    Fix PDO error mode constant

--- config/database.php
+++ config/database.php
@@ -24,7 +24,7 @@ class Database {
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
             echo "Error de conexión: " . $exception->getMessage();
         }
